fe709e697f444d4c5d35c8e0a70f905d9cb940c4
Jakareya <76894384+Jakareya76@users.noreply.github.com>
2022-02-06 18:32:26 +0600

    Update About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,4 +1,4 @@
-import React,{useEffect ,useState} from 'react';
+import {useEffect ,useState} from 'react';
 import userImg from './assets/user.jpg';
 import {Link} from 'react-router-dom';
 import {useHistory} from 'react-router-dom';
